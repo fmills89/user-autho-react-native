6e6dff14d0ef133bc85e7ec35e0cf1a6939829fa
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-08-03 10:42:44 -0500

    added shared loging to autho - login user

--- util/auth.js
+++ util/auth.js
@@ -2,16 +2,25 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyC-eVBA69tzwYI75xlUtmItTa9_LT1TvzI';
 
+// shared function that has main logic
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 // by using async/await we are make sure func overrall returns promise that will
 // resolve once request is done - allowing for loading overlay
 export async function createUser(email, password) {
-  const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-    // argument is object then converted to json by axios
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    }
-  );
+  await authenticate('signup', email, password);
+}
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password);
 }
